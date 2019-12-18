73a01abc7f51e39267707a7ed4208bf0a20aa2f0
Konstantin Web <47927228+ykosweb@users.noreply.github.com>
2019-12-18 22:17:42 +0200

    end

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 import style from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
+import {onNewMessageChangeActionCreator, onNewMessageSendActionCreator} from "../../redux/state";
 
 const Dialogs = (props) => {
 
     let dialogsElements = props.state.dialogs.map( dialog => <DialogItem name={dialog.name} id={dialog.id}/> );
-
     let messagesElements = props.state.messages.map( msg => <Message message={msg.message}/>);
 
     const newMessage = React.createRef()
 
     const onNewMessageSend = () => {
-        let text = newMessage.current.value;
-        alert(text);
+        props.dispatch(onNewMessageSendActionCreator());
     };
+
+    const onMessageEdit = () => {
+        let text = newMessage.current.value;
+        props.dispatch(onNewMessageChangeActionCreator(text));
+    }
 
     return (
         <div className={style.dialogs}>
@@ -25,7 +29,7 @@ const Dialogs = (props) => {
                 {messagesElements}
             </div>
             <div>
-                <textarea ref={newMessage}></textarea>
+                <textarea ref={newMessage} onChange={onMessageEdit}></textarea>
                 <button onClick={onNewMessageSend}>Send</button>
             </div>
         </div>
